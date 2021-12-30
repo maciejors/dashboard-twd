9c6adb36b7cf8842274acf7684c75d44a5bb4d8a
meagle <maciej.orslowski@gmail.com>
2021-12-30 23:05:51 +0100

    add spotipy to requirements.txt & add comments to app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import dash
 from dash import dcc
 from dash import html
+from dash.dependencies import Input, Output
+
+from utils.readingfiles import *
 
 
 app = dash.Dash(__name__)
@@ -14,7 +17,29 @@ app.layout = html.Div([
         accept=".zip",
         multiple=True,
     ),
+    # jak już będziemy mieli wykresy to tutaj się wstawi ich id:
+    # dcc.Graph(id="test-plot1"),
+    # dcc.Graph(id="test-plot2"),
+    # ...
 ])
+
+# Wtedy te id wrzuci się tu:
+#
+# @app.callback(
+#     Output("test-plot1", "figure"),
+#     Input("upload-data", "contents"))
+# def update_test_plot1(file_contents):
+#     df = get_streaming_history(parse_zip(file_contents))
+#     return test_plot1(df)
+#
+# @app.callback(
+#     Output("test-plot2", "figure"),
+#     Input("upload-data", "contents"))
+# def update_test_plot2(file_contents):
+#     df = get_streaming_history(parse_zip(file_contents))
+#     return test_plot2(df)
+#
+# ...
 
 if __name__ == '__main__':
     app.run_server(debug=True)
