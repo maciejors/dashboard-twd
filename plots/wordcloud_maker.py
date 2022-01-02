39752296870f030ee42a99562fb20c85725ea49b
kasprzakj <kasprzakkuba01@gmail.com>
2022-01-02 23:33:55 +0100

    change stopwords to .txt based rather than library generated

--- plots/wordcloud_maker.py
+++ plots/wordcloud_maker.py
@@ -1,6 +1,41 @@
-from nltk.corpus import stopwords
-from stop_words import get_stop_words
+# from nltk.corpus import stopwords
+# from stop_words import get_stop_words
 from wordcloud import WordCloud
+
+
+# def write_stopwords_to_file():
+#     languages = stopwords.fileids()
+#     sw = set()
+#     for language in languages:
+#         sw.update(stopwords.words(language))
+#     languages = ['arabic',
+#                  'bulgarian',
+#                  'catalan',
+#                  'czech',
+#                  'danish',
+#                  'dutch',
+#                  'english',
+#                  'finnish',
+#                  'french',
+#                  'german',
+#                  'hungarian',
+#                  'indonesian',
+#                  'italian',
+#                  'norwegian',
+#                  'polish',
+#                  'portuguese',
+#                  'romanian',
+#                  'russian',
+#                  'spanish',
+#                  'swedish',
+#                  'turkish',
+#                  'ukrainian']
+#     for language in languages:
+#         sw.update(get_stop_words(language))
+#     sw.add("ft")
+#     print(repr(sw))
+#     with open("../assets/sw.txt", "w", encoding="utf-8") as f:
+#         f.write(repr(sw))
 
 
 def create_wordcloud(words_to_cloud):
@@ -8,36 +43,10 @@ def create_wordcloud(words_to_cloud):
     Ta funkcja wypluwa chmurę słów, to 'words_to_cloud' to ramka danych najprawdopodobniej
     podana przez get_lyrics(get_streaming_history(zipped_data)))
     """
-    languages = stopwords.fileids()
-    sw = set()
-    for language in languages:
-        sw.update(stopwords.words(language))
-    languages = ['arabic',
-                 'bulgarian',
-                 'catalan',
-                 'czech',
-                 'danish',
-                 'dutch',
-                 'english',
-                 'finnish',
-                 'french',
-                 'german',
-                 'hungarian',
-                 'indonesian',
-                 'italian',
-                 'norwegian',
-                 'polish',
-                 'portuguese',
-                 'romanian',
-                 'russian',
-                 'spanish',
-                 'swedish',
-                 'turkish',
-                 'ukrainian']
-    for language in languages:
-        sw.update(get_stop_words(language))
-    sw.add("ft")
-    return WordCloud(stopwords=sw, background_color="white").generate(words_to_cloud)
+
+    with open("./assets/sw.txt", "r", encoding = "utf-8") as file:
+        sw = eval(file.read())
+        return WordCloud(stopwords=sw, background_color="white").generate(words_to_cloud)
 
 # Tak to testowałem, wrzucam żeby na potem było wiadomo co tam dodatkowo trzeba dopisać przy
 # printowaniu żeby ładnie wyglądało
