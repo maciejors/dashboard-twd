68bf85d4cf6b3d0889433737658bcfdff688d0e2
meagle <maciej.orslowski@gmail.com>
2022-01-03 10:13:42 +0100

    add plots to app.py

--- app.py
+++ app.py
@@ -1,45 +1,116 @@
+import io
+import base64
+
 import dash
+import pandas as pd
+import matplotlib.pyplot as plt
+import wordcloud
 from dash import dcc
 from dash import html
 from dash.dependencies import Input, Output
 
-from utils.readingfiles import *
+from utils.decorators import all_args_none
+from utils.readingfiles import parse_zip, get_streaming_history
+from utils.lyrics_getter import get_lyrics
 
+from plots.favourite_artist import favourite_artist
+from plots.song_most_skipped import most_skipped
+from plots.when_listening_dist import when_listening_dist
+from plots.wordcloud_maker import create_wordcloud
 
 app = dash.Dash(__name__)
 server = app.server
 
 app.layout = html.Div([
-    html.H1(children="siema"),
     dcc.Upload(
         id="upload-data",
         children=html.Button("Upload zipped Spotify data"),
         accept=".zip",
-        multiple=True,
+        multiple=False,
     ),
-    # jak już będziemy mieli wykresy to tutaj się wstawi ich id:
-    # dcc.Graph(id="test-plot1"),
-    # dcc.Graph(id="test-plot2"),
-    # ...
+    dcc.Store(id="streaming-history-storage"),
+    html.Div(id="favourite-artist"),
+    html.Br(),
+    html.Div(id="most-skipped"),
+    html.Br(),
+    html.Div(id="when-listening"),
+    html.Br(),
+    html.Div(id="wordcloud"),
 ])
 
-# Wtedy te id wrzuci się tu:
-#
-# @app.callback(
-#     Output("test-plot1", "figure"),
-#     Input("upload-data", "contents"))
-# def update_test_plot1(file_contents):
-#     df = get_streaming_history(parse_zip(file_contents))
-#     return test_plot1(df)
-#
-# @app.callback(
-#     Output("test-plot2", "figure"),
-#     Input("upload-data", "contents"))
-# def update_test_plot2(file_contents):
-#     df = get_streaming_history(parse_zip(file_contents))
-#     return test_plot2(df)
-#
-# ...
+
+@app.callback(
+    Output("streaming-history-storage", "data"),
+    Input("upload-data", "contents"))
+@all_args_none(default_val=None)
+def update_streaming_history(file_contents):
+    streaming_history = get_streaming_history(parse_zip(file_contents))
+    return streaming_history.to_dict()
+
+
+@app.callback(
+    Output("favourite-artist", "children"),
+    Input("streaming-history-storage", "data"))
+@all_args_none(default_val=None)
+def update_favourite_artist(streaming_history):
+    streaming_history = pd.DataFrame(streaming_history)
+    artist_name, img_url = favourite_artist(streaming_history)
+    return [
+        html.H4(f"Most listened to artist: {artist_name}"),
+        html.Img(
+            alt=f"Image of {artist_name}",
+            src=img_url
+        ),
+    ]
+
+
+@app.callback(
+    Output("most-skipped", "children"),
+    Input("streaming-history-storage", "data"))
+@all_args_none(default_val=None)
+def update_most_skipped(streaming_history):
+    streaming_history = pd.DataFrame(streaming_history)
+    fig = most_skipped(streaming_history)
+    plt.figure(fig.number)
+    img = io.BytesIO()
+    plt.savefig(img, format="png")
+    plt.close()
+    data = base64.b64encode(img.getvalue()).decode("utf8")
+    return html.Img(
+        src=f"data:image/png;base64,{data}",
+        alt="most skipped track"
+    )
+
+
+@app.callback(
+    Output("when-listening", "children"),
+    Input("streaming-history-storage", "data"))
+@all_args_none(default_val=None)
+def update_when_listening(streaming_history):
+    streaming_history = pd.DataFrame(streaming_history)
+    return dcc.Graph(figure=when_listening_dist(streaming_history))
+
+
+@app.callback(
+    Output("wordcloud", "children"),
+    Input("streaming-history-storage", "data"))
+@all_args_none(default_val=None)
+def update_wordcloud(streaming_history):
+    number_of_tracks = 10  # później mozna dodac zeby to sie wybieralo interaktywnie
+    streaming_history = pd.DataFrame(streaming_history)
+    wcloud: wordcloud.WordCloud = create_wordcloud(
+        get_lyrics(
+            streaming_history.tail(number_of_tracks)
+        )
+    )
+    img = io.BytesIO()
+    wcloud.to_image().save(img, format="png")
+    data = base64.b64encode(img.getvalue()).decode("utf8")
+    return html.Img(
+        src=f"data:image/png;base64,{data}",
+        alt="Wordcloud of words in lyrics"
+    )
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
